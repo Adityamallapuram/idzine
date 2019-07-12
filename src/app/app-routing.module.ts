cc2c56098a835cc3bb98d845ec3e4c91688087f2
Adityamallapuram <mallapuramaditya@gmail.com>
2019-07-12 12:54:42 +0530

    first commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,34 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { HomeComponent } from './components/home/home.component';
+import { ContactComponent } from './components/contact/contact.component';
+import { BedroomComponent } from './components/gallery/bedroom/bedroom.component';
+import { KitchenComponent } from './components/gallery/kitchen/kitchen.component';
+import { LivingComponent } from './components/gallery/living/living.component';
+import { WardrobeComponent } from './components/gallery/wardrobe/wardrobe.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+
+  { path: '', component: HomeComponent },
+  { path: 'kitchen', component: KitchenComponent },
+  { path: 'bedroom', component: BedroomComponent },
+  { path: 'living', component: LivingComponent },
+  { path: 'wardrobe', component: WardrobeComponent },
+  { path: 'contact', component: ContactComponent }
+
+
+
+
+
+
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {
+
+
+}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,71 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
+
+// import {
+//   MatButtonModule,
+//   MatCheckboxModule,
+//   MatToolbarModule,
+//   MatSidenavModule, MatIconModule, MatListModule
+
+// } from '@angular/material';
+
+
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { AboutComponent } from './components/about/about.component';
+import { ServicesComponent } from './components/services/services.component';
+import { ContactComponent } from './components/contact/contact.component';
+import { FooterComponent } from './components/footer/footer.component';
+import { HomeComponent } from './components/home/home.component';
+import { GalleryComponent } from './components/gallery/gallery.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
+// import { LayoutModule } from '@angular/cdk/layout';
+import { KitchenComponent } from './components/gallery/kitchen/kitchen.component';
+import { LivingComponent } from './components/gallery/living/living.component';
+import { BedroomComponent } from './components/gallery/bedroom/bedroom.component';
+import { WardrobeComponent } from './components/gallery/wardrobe/wardrobe.component';
+// import { AngularFontAwesomeModule } from 'angular-font-awesome';
+// import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+// import { NgbCollapseModule } from '@ng-bootstrap/ng-bootstrap';
+import { WhatwedoComponent } from './components/whatwedo/whatwedo.component';
+
+
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AboutComponent,
+    ServicesComponent,
+    ContactComponent,
+    FooterComponent,
+    HomeComponent,
+    GalleryComponent,
+    NavbarComponent,
+    KitchenComponent,
+    LivingComponent,
+    BedroomComponent,
+    WardrobeComponent,
+    WhatwedoComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    BrowserAnimationsModule,
+    AppRoutingModule,
+    // MatToolbarModule,
+    // MatCheckboxModule,
+    // MatButtonModule,
+    // MatSidenavModule,
+    // LayoutModule,
+    // MatIconModule,
+    // MatListModule,
+    // AngularFontAwesomeModule,
+    // NgbModule,
+    // NgbCollapseModule
+
   ],
   providers: [],
   bootstrap: [AppComponent]
